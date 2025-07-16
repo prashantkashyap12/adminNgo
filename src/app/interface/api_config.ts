3d2714c42310d1e8bd4f5091d0851f0a9c78fce9
prashantkashyap12 <prashantkashyap12@gmail.com>
2025-07-16 07:43:36 +0530

    auth_config

--- src/app/interface/api_config.ts
+++ src/app/interface/api_config.ts
@@ -1,5 +1,5 @@
 export class url{
     // public value:string = "https://b5gb6nvz-5181.inc1.devtunnels.ms/"; // omr 
-    public value:string = "http://localhost:5181/";  // Local
+    public value:string = "https://j1x5zj0t-5181.inc1.devtunnels.ms/";    // Local
 }
 
